cbe11b2fc43f67ce2817215105f5ea196d5cd977
xskutsu <xsku@protonmail.com>
2025-09-07 22:53:26 -0500

    readonly globals

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -1,36 +1,36 @@
 declare class Howl {
-	public _volume: number;
-	public _src: string | string[];
+	public readonly _volume: number;
+	public readonly _src: string;
 	public constructor(options?: { src: string | string[], volume: number });
 	public play(sprite?: string, callback?: () => void): number;
 	public volume(volume: number, id?: number): void;
 }
 
 interface Rv20SoundStrings {
-	discDeath0: string,
-	discDeath1: string,
-	discDeath2: string,
-	discDisc: string,
-	discOffScreen: string,
-	platBounce: string,
-	capIncrease: string,
-	capDecrease: string,
-	capComplete: string,
-	footKick: string,
-	footBounce: string,
-	footGoal: string,
-	scoreAnimation: string,
-	classicButtonClick: string,
-	classicButtonHover: string,
-	classicButtonBack: string,
-	listRowHover: string,
-	listRowClick: string,
-	popNote: string,
-	digiBeep: string
+	readonly discDeath0: string,
+	readonly discDeath1: string,
+	readonly discDeath2: string,
+	readonly discDisc: string,
+	readonly discOffScreen: string,
+	readonly platBounce: string,
+	readonly capIncrease: string,
+	readonly capDecrease: string,
+	readonly capComplete: string,
+	readonly footKick: string,
+	readonly footBounce: string,
+	readonly footGoal: string,
+	readonly scoreAnimation: string,
+	readonly classicButtonClick: string,
+	readonly classicButtonHover: string,
+	readonly classicButtonBack: string,
+	readonly listRowHover: string,
+	readonly listRowClick: string,
+	readonly popNote: string,
+	readonly digiBeep: string
 }
 
 declare class GameResources {
-	static svgStrings: [null, ...string[]];
-	static soundStrings: Rv20SoundStrings;
-	static bowSVG: string;
+	static readonly svgStrings: [null, ...string[]];
+	static readonly soundStrings: Rv20SoundStrings;
+	static readonly bowSVG: string;
 }
